f903bfda4ff24b7dde1bdbe8fe425584dbda74dc
Swrh1 <sawyerhammond14@gmail.com>
2023-05-07 20:13:50 -0500

    fixed dist

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dist/',
+  base: '/cg-illumination/',
   plugins: [vue()],
   resolve: {
     alias: {
